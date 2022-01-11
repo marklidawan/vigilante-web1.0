5f36dc03a0d9a8fb6f818e2d0f46ead676fc1092
marklidawan <marklidawan@gmail.com>
2022-01-11 14:20:02 +0800

    api validation

--- add_user.php
+++ add_user.php
@@ -11,7 +11,18 @@
     $contactNumber = isset($_GET['contact_number']) ? $_GET['contact_number'] : null;
     $emergencyNumber = isset($_GET['emergency_number']) ? $_GET['emergency_number'] : null;
 
-    $query = "INSERT INTO user (email, password, fname, mname, lname, gender, address, contactNumber,emergencyNumber) VALUES ('$email','$password','$fname','$mname','$lname','$gender','$address','$contactNumber','$emergencyNumber')";
-    mysqli_query($conn, $query);
-    echo json_encode(array('status' => "Successfully created report"));
+    $sql = "SELECT email,password FROM user WHERE email ='$email' AND password='$password'";
+
+    $res = mysqli_query($conn, $sql);
+    $count = mysqli_num_rows($res);
+
+    if($count==1){
+        echo json_encode("Error");
+    }else{
+        $query = "INSERT INTO user (email, password, fname, mname, lname, gender, address, contactNumber,emergencyNumber) VALUES ('$email','$password','$fname','$mname','$lname','$gender','$address','$contactNumber','$emergencyNumber')";
+        $insert = mysqli_query($conn, $query);
+        if($insert){
+            echo json_encode(array('status' => "Success"));
+        }
+    }
 ?>
--- login_user.php
+++ login_user.php
@@ -5,6 +5,13 @@
     $password = isset($_GET['password']) ? $_GET['password'] : null;
 
     $query = "SELECT email,password FROM user WHERE email = '$email' and password = '$password'";
-    mysqli_query($conn, $query);
-    echo json_encode(array('status' => "Success"));
+
+    $res = mysqli_query($conn,$query);
+    $count = mysqli_num_rows($res);
+
+    if($count==1){
+        echo json_encode(array('status' => "Success"));
+    }else{
+        echo json_encode("Error");
+    }
 ?>
